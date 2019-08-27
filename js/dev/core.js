af8e87753eb8f293cde003313d14d5aa90ababa7
Aneesh Barthakur <abarthakur96@gmail.com>
2019-08-27 10:15:59 +0530

    Edit js/dev/core.js ** refactor (& fix?) handler for widget wall tag controls. ** add startup code that checks if num posts is correct for screen size and reloads the widget default query if required

--- js/dev/core.js
+++ js/dev/core.js
@@ -1,10 +1,27 @@
 window.onload = function(){
+	//Startup code 
+
 	$(".clamped-text-container").each( function (index){
 		//TODO : Replace 16 with package.json param
 		var staticHeight=$(this).parent().height() - 16;
 		$clamp(this,{clamp:staticHeight.toString()+"px",useNativeClamp:false,nukeElements:false});
 	});
 
+	//check if default query matches current number of posts or else load more
+	$(".preview-post-wall").each( function(index,value){
+		var url=$(".default.pwall-tag-link:first",$(this)).attr("href");
+		url=parsePWallUrl(url);
+		//matche regex and get count
+		var reg= /\?count=(\d*)/i;
+		var matches=reg.exec(url);
+		var count=parseInt(matches[1]);
+		if ($(".post-col",$(this)).length!=count){
+			loadQueryIntoWall(url,$(this));
+		}
+
+	});
+
+	//Handlers
 	$(".place-tabbed-pane [role='tab']").click(function(event){
 		var selected=$(this).attr('aria-selected');
 		if (selected!="true" && selected!="false"){
@@ -34,42 +51,120 @@ window.onload = function(){
 
 	$(".taglist .pwall-tag-link").on("click", function(event){
 		event.preventDefault();
-		var $this=$(this);
-		var url="";
-		if ($this.hasClass("default") && $this.hasClass("active")){
+		var activeChanged=changeActiveTag($(this));
+		if (!activeChanged){
 			return;
 		}
-		if ($this.hasClass("active")){
-			$this.removeClass("active");
-			var default_tag = $this.siblings(".default");
-			url=$(default_tag).attr("href");
-			$(default_tag).addClass("active");
-		}
-		else{
-			$this.siblings(".pwall-tag-link").removeClass("active");
-			url=$this.attr("href");
-			$this.addClass("active");
-		}
-		$.get(url,function(data,status,jqxhr){
-			var html=$.parseHTML(data);
-			var $results=$(".post-preview-card",html);
-			var $wall=$($this.closest(".preview-post-wall")[0]);
-			var $wallrow=$($(".posts-container .row",$wall)[0]);
-			var colClasses=$wallrow.children(".post-col")[0].classList;
-			console.log(colClasses);
-			$wallrow.children(".post-col").remove();
-			$results.each(function(index,el){
-				var newCol=document.createElement("div");
-				$(newCol).addClass(colClasses.value);
-				newCol.appendChild(el);
-				$wallrow.append(newCol);
-			});
-
-			$wall.find(".clamped-text-container").each( function (index){
-				//TODO : Replace 16 with package.json param
-				var staticHeight=$(this).parent().height() - 10;
-				$clamp(this,{clamp:staticHeight.toString()+"px",useNativeClamp:false,nukeElements:false});
-			});
-		});
+		var $wall=$($(this).closest(".preview-post-wall")[0]);
+		var activeTag=$(".pwall-tag-link.active",$wall)[0];
+		var url=$(activeTag).attr("href");
+		url = parsePWallUrl(url);
+		loadQueryIntoWall(url,$wall);
 	});
 };
+
+
+function changeActiveTag($clicked){
+	//the default tag has been clicked, while active
+	if ($clicked.hasClass("default") && $clicked.hasClass("active")){
+		//nothing to do
+		return false;
+	}
+	//some non defualt, active tag is clicked
+	else if ($clicked.hasClass("active")){
+		//make "default" tag as active
+		$clicked.removeClass("active");
+		var defaultTag = $clicked.siblings(".default");
+		$(defaultTag).addClass("active");
+		return true;
+	}
+	//some inactive tag is clicked
+	else{
+		//make this tag as active
+		$clicked.siblings(".pwall-tag-link").removeClass("active");
+		$clicked.addClass("active");
+		return true;
+	}
+}
+
+
+function parsePWallUrl(url){
+	var urlParts=url.split("?");
+	var countError=new Error("Tag wall url "+url+" doesn't specify count");
+	//url must has a query string
+	if (urlParts.length<1){
+		throw countError;
+	}
+	var query=urlParts[1];
+	//separate other query params from count params
+	var params=query.split("&");
+	var countParams=[];
+	var otherParams=[];
+	for (var i=0;i<params.length;i++){
+		if (params[i].startsWith("count")){
+			countParams.push(params[i]);
+		}
+		else{
+			otherParams.push(params[i]);
+		}
+	}
+	//query string must contain at least one query string parameter
+	if (countParams.length<1){
+		throw countError;
+	}
+
+	// get the count for the largest min-width parameter that matches. 
+	// For ex, for count, count_440, count_768, if the min-width is 550, 
+	// we must send count_440's value as the count parameter.
+	var count=0;
+	var maxMinWidth=-1;
+	var parMinWidth=0;
+	for (var i=0;i<countParams.length;i++){
+		var pair=countParams[i].split("=");
+		var cparts=pair[0].split("_");
+		if (cparts.length==1){//default count
+			parMinWidth=0;
+		}
+		else if (cparts.length==2){
+			parMinWidth=parseInt(cparts[1]);
+		}
+		else{
+			throw new Error ("Invalid count parameter "+pair[0])
+		}
+		if (parMinWidth > maxMinWidth &&
+			window.matchMedia("(min-width: "+parMinWidth.toString()+"px)").matches){
+			count=parseInt(pair[1]);
+			maxMinWidth=parMinWidth;
+		}
+	}
+	//construct the new url
+	var newUrl = urlParts[0]+"?";
+	newUrl += "count=" + count.toString()
+	for (var i=0; i<otherParams.length;i++){
+		newUrl+="&"+otherParams[i];
+	}
+	return newUrl;
+}
+
+
+function loadQueryIntoWall(url,$wall){
+	$.get(url,function(data,status,jqxhr){
+		var html=$.parseHTML(data);
+		var $results=$(".post-preview-card",html);
+		var $wallrow=$($(".posts-container .row",$wall)[0]);
+		var colClasses=$wallrow.children(".post-col")[0].classList;
+		$wallrow.children(".post-col").remove();
+		$results.each(function(index,el){
+			var newCol=document.createElement("div");
+			$(newCol).addClass(colClasses.value);
+			newCol.appendChild(el);
+			$wallrow.append(newCol);
+		});
+
+		$wall.find(".clamped-text-container").each( function (index){
+			//TODO : Replace 16 with package.json param
+			var staticHeight=$(this).parent().height() - 10;
+			$clamp(this,{clamp:staticHeight.toString()+"px",useNativeClamp:false,nukeElements:false});
+		});
+	});
+}
