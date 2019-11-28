ddb29990e78c94a7521042735c35b58d6a196329
Aneesh Barthakur <abarthakur96@gmail.com>
2019-11-28 21:05:41 +0530

    Edit single.php ** Rewrite as two col layout using css/sass/layout/_layout.scss classes instead of BS4.

--- single.php
+++ single.php
@@ -3,21 +3,23 @@
 get_header();
 ?>
 
-<div class="top-container">
-	<div class="row top-page-row">
-		<div class="col-lg-8 col-md col-sm content-col">
-		<?php
-			if (have_posts()){
-				while(have_posts()){
-					the_post();
-					get_template_part('partials/post');
+<div class="page-section-wrap">
+	<div class="page-section-inner">
+		<div class="two-col-container">
+			<div class="main-col">
+			<?php
+				if (have_posts()){
+					while(have_posts()){
+						the_post();
+						get_template_part('partials/post');
+					}
 				}
-			}
-		?>
-		</div>
-		<div class="col-lg-4 col-md col-dm sidebar-col">
-			<div class="sidebar-wrapper">
-				<?php get_template_part('partials/sidebar'); ?>
+			?>
+			</div>
+			<div class="side-col">
+				<div class="sidebar-wrapper">
+					<?php get_template_part('partials/sidebar'); ?>
+				</div>
 			</div>
 		</div>
 	</div>
